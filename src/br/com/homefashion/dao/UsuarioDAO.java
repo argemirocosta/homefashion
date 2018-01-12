e55d6b86967204aeb1b4f928a53e91a979903b8c
argemirocosta <argemirocosta@outlook.com>
2018-01-12 20:52:26 -0300

    Gravando o id do usuário na sessão.

--- src/br/com/homefashion/dao/UsuarioDAO.java
+++ src/br/com/homefashion/dao/UsuarioDAO.java
@@ -2,19 +2,24 @@ package br.com.homefashion.dao;
 
 import br.com.homefashion.connection.ConnectionFactory;
 import br.com.homefashion.model.Usuario;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.faces.context.FacesContext;
 
 public class UsuarioDAO {
 
 	private Connection conexao;
 
 	public Usuario login(Usuario usuario) {
-
+		
+		FacesContext fc = FacesContext.getCurrentInstance();
+		
 		conexao = ConnectionFactory.getConnection();
-
+		
 		String sql = "select id, nome, login, senha, ativo from vendas.usuario where login = ? and senha = ?";
 
 		Usuario u = null;
@@ -32,7 +37,12 @@ public class UsuarioDAO {
 				u.setLogin(rs.getString("login"));
 				u.setSenha(rs.getString("senha"));
 				u.setAtivo(rs.getBoolean("ativo"));
+				
 			}
+			
+			//Armazenar id na sessão
+			fc.getExternalContext().getSessionMap().put("usuario",u.getId());
+			
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
