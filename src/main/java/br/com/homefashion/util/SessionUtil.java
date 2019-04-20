c64365c13abade335782ab0e061f40e98297fa57
Argemiro <argemiro.pcosta@gmail.com>
2019-04-20 20:57:32 -0300

    - Convertendo o projeto para maven.

---
+++ src/main/java/br/com/homefashion/util/SessionUtil.java
@@ -0,0 +1,13 @@
+package br.com.homefashion.util;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+public class SessionUtil {
+
+	public static HttpSession getSession() {
+		ExternalContext extCon = FacesContext.getCurrentInstance().getExternalContext();
+		return (HttpSession) extCon.getSession(true);
+	}
+}
