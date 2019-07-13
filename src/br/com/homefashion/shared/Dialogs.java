2863ddc4bdfedcba9a1e2e7846d2aefea06f06c8
Argemiro <argemiro.pcosta@gmail.com>
2019-07-13 01:52:24 -0300

    - Alterar nome dos dialogs. - Atualizar tags depreciadas no index.

--- src/br/com/homefashion/shared/Dialogs.java
+++ src/br/com/homefashion/shared/Dialogs.java
@@ -4,15 +4,15 @@ public class Dialogs {
 
     private Dialogs() {}
 
-    public static final String DIALOG_CADASTRAR_CLIENTE = "dlgCadCliente";
+    public static final String DIALOG_CADASTRAR_CLIENTE = "dlgCadastrarCliente";
 
-    public static final String DIALOG_DELETAR_CLIENTE = "dlgDeleteCliente";
+    public static final String DIALOG_DELETAR_CLIENTE = "dlgDeletarCliente";
 
-    public static final String DIALOG_ALTERAR_CLIENTE = "dlgAltCliente";
+    public static final String DIALOG_ALTERAR_CLIENTE = "dlgAlterarCliente";
 
     public static final String DIALOG_VENDER = "dlgVender";
 
-    public static final String DIALOG_CADASTRO_USUARIO = "dlgCadastro";
+    public static final String DIALOG_CADASTRO_USUARIO = "dlgCadastroUsuario";
 
 
 }
