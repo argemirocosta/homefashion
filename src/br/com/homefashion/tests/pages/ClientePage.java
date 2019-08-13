670c7993537b87b644f6cdcaa5878ce4060de968
Argemiro <argemiro.pcosta@gmail.com>
2019-08-13 08:22:20 -0300

    - Implementação do findBy e do padrão de projeto Page Factory nos testes relativos ao cliente.

--- src/br/com/homefashion/tests/pages/ClientePage.java
+++ src/br/com/homefashion/tests/pages/ClientePage.java
@@ -1,36 +1,32 @@
 package br.com.homefashion.tests.pages;
 
-import br.com.homefashion.tests.util.TesteUtil;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import static br.com.homefashion.tests.elements.ClienteElements.*;
 
 public class ClientePage {
 
-    private final WebDriver driver;
+    @FindBy(name = CLIENTE_BOTAO_ABRIR_DIALOG_CADASTRO_CLIENTE)
+    private WebElement botaoCadastro;
 
-
-    public ClientePage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    public void abrirDialogNovoCliente(){
-        WebElement botaoCadastro = TesteUtil.buscarComponentePorNome(CLIENTE_BOTAO_ABRIR_DIALOG_CADASTRO_CLIENTE, driver);
+    public void abrirDialogNovoCliente() {
         botaoCadastro.click();
     }
 
-    public void fecharDialogCadastro(){
-        WebElement botaoFecharDialogCadastro = TesteUtil.buscarComponentePorNome(CLIENTE_BOTAO_ABRIR_DIALOG_CADASTRO_CLIENTE, driver);
-        botaoFecharDialogCadastro.click();
-    }
+    @FindBy(name = CLIENTE_INPUT_NOME)
+    private WebElement campoNome;
 
-    public void realizarCadastroCliente(String nome, Integer telefone1, Integer telefone2){
-        WebElement campoNome = TesteUtil.buscarComponentePorNome(CLIENTE_INPUT_NOME, driver);
-        WebElement campoTelefone1 = TesteUtil.buscarComponentePorNome(CLIENTE_INPUT_TELEFONE1, driver);
-        WebElement campoTelefone2 = TesteUtil.buscarComponentePorNome(CLIENTE_INPUT_TELEFONE2, driver);
-        WebElement botaoCadastrar = TesteUtil.buscarComponentePorNome(CLIENTE_BOTAO_CADASTRAR_CLIENTE, driver);
+    @FindBy(name = CLIENTE_INPUT_TELEFONE1)
+    private WebElement campoTelefone1;
 
+    @FindBy(name = CLIENTE_INPUT_TELEFONE2)
+    private WebElement campoTelefone2;
+
+    @FindBy(name = CLIENTE_BOTAO_CADASTRAR_CLIENTE)
+    private WebElement botaoCadastrar;
+
+    public void realizarCadastroCliente(String nome, Integer telefone1, Integer telefone2) {
         campoNome.sendKeys(nome);
         campoTelefone1.sendKeys(String.valueOf(telefone1));
         campoTelefone2.sendKeys(String.valueOf(telefone2));
@@ -38,5 +34,4 @@ public class ClientePage {
         botaoCadastrar.click();
 
     }
-
 }
--- src/br/com/homefashion/tests/tests/ClienteTest.java
+++ src/br/com/homefashion/tests/tests/ClienteTest.java
@@ -9,6 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import static br.com.homefashion.tests.elements.ClienteElements.*;
 import static org.junit.Assert.assertTrue;
@@ -20,24 +21,27 @@ public class ClienteTest {
 
     private static WebDriver driver;
     private static IndexPage indexPage;
-    private static ClientePage clientePage;
+
+    public ClienteTest() {
+        ClientePage page = PageFactory.initElements(driver, ClientePage.class);
+    }
 
     @Before
     public void inicializa() {
         driver = FactoryDriver.createDriver(NOME_DO_NAVEGADOR);
         driver.get(PAGINA_PARA_TESTE);
         indexPage = new IndexPage(driver);
-        clientePage = new ClientePage(driver);
     }
 
     @Test
     public void deveCadastrarCliente() {
+        ClientePage page = PageFactory.initElements(driver, ClientePage.class);
 
         indexPage.realizarLogin(USUARIO_TESTE_LOGIN, SENHA_TESTE_LOGIN);
 
-        clientePage.abrirDialogNovoCliente();
+        page.abrirDialogNovoCliente();
 
-        clientePage.realizarCadastroCliente(FakerUtil.gerarNome(), FakerUtil.gerarTelefone(), FakerUtil.gerarTelefone());
+        page.realizarCadastroCliente(FakerUtil.gerarNome(), FakerUtil.gerarTelefone(), FakerUtil.gerarTelefone());
 
         boolean resultadoTeste = TesteUtil.encontrouTexto(TEXTO_SUCESSO_CADASTRO_CLIENTE, driver);
 
