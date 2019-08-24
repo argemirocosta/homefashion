fed46c91e397a566c8551ce851f8197e05819346
Argemiro <argemiro.pcosta@gmail.com>
2019-08-24 16:28:40 -0300

    - Criando testes parametrizados para a classe DocumentoUtil.

---
+++ test/br.com.homefashion/util/DocumentosUtilTeste.java
@@ -0,0 +1,43 @@
+package br.com.homefashion.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class DocumentosUtilTeste {
+
+    @Parameterized.Parameter
+    public String cpf;
+
+    @Parameterized.Parameter(value = 1)
+    public String cenario;
+
+    @Parameterized.Parameters(name = "{1}")
+    public static Collection parametros() {
+        String cpf1Correto = "25623561527";
+        String cpf2Correto = "41329663217";
+        String cpf1Errado = "11111111111";
+        String cpf2Errado = "12345678912";
+        return Arrays.asList(new Object[][] {
+                {cpf1Errado, "Número Iguais"},
+                {cpf2Errado, "CPF Errado"},
+                {cpf1Correto, "CPF Correto"},
+                {cpf2Correto, "CPF Correto"}
+        });
+    }
+
+    @Test
+    public void testarSeCpfEhValido() {
+        if(cenario.contains("Correto"))
+            assertTrue(DocumentosUtil.validaCPF(cpf));
+        else
+            assertFalse(DocumentosUtil.validaCPF(cpf));
+    }
+}
