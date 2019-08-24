b5b19f92cba3df62ce7e9927a54e87ba291b2454
Argemiro <argemiro.pcosta@gmail.com>
2019-08-24 17:19:33 -0300

    - Criação de suíte de teste para as classe do pacote util.

---
+++ test/suites/SuitesUtil.java
@@ -0,0 +1,28 @@
+package suites;
+
+import br.com.homefashion.util.DataUtilTeste;
+import br.com.homefashion.util.DocumentosUtilTeste;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        DataUtilTeste.class,
+        DocumentosUtilTeste.class
+})
+
+public class SuitesUtil {
+
+    @BeforeClass
+    public static void iniciarSuiteDeTeste(){
+        System.out.println("Iniciando classe ");
+    }
+
+    @AfterClass
+    public static void finalizarSuiteDeTeste(){
+        System.out.println("Finalizando classe");
+    }
+
+}
