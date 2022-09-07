ceb0801c966c58482be1077d078988374674a7f0
Argemiro github <argemirocosta@outlook.com>
2022-09-07 11:38:09 -0300

    Retirando dependências com problemas de segurança

--- src/main/br/com/homefashion/util/CEPUtil.java
+++ src/main/br/com/homefashion/util/CEPUtil.java
@@ -2,22 +2,59 @@ package br.com.homefashion.util;
 
 import br.com.homefashion.model.Endereco;
 
-public final class CEPUtil {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public static Endereco buscarEnderecoPorCEP(String cep) {
-        Endereco endereco = new Endereco();
+public final class CEPWebService {
 
-        CEPWebService cepWebService = new CEPWebService(cep);
+	private static String pesquisarCepNaViaCep(String cep) {
+		String json;
 
-        if (cepWebService.getCodIBGE() != 0) {
-            endereco.setLogradouro(cepWebService.getLogradouro());
-            endereco.setEstado(cepWebService.getEstado());
-            endereco.setCidade(cepWebService.getCidade());
-            endereco.setBairro(cepWebService.getBairro());
-            endereco.setCodIBGE(cepWebService.getCodIBGE());
-        }
+		try {
+			URL url = new URL("http://viacep.com.br/ws/"+ cep +"/json");
+			URLConnection urlConnection = url.openConnection();
+			InputStream is = urlConnection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-        return endereco;
-    }
+			StringBuilder jsonSb = new StringBuilder();
+
+			br.lines().forEach(l -> jsonSb.append(l.trim()));
+
+			json = jsonSb.toString();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		return json;
+	}
+
+	public static Endereco buscarCep(String cep) {
+		String json = pesquisarCepNaViaCep(cep);
+
+		Map<String,String> mapa = new HashMap<>();
+
+		Matcher matcher = Pattern.compile("\"\\D.*?\": \".*?\"").matcher(json);
+		while (matcher.find()) {
+			String[] group = matcher.group().split(":");
+			mapa.put(group[0].replaceAll("\"", "").trim(), group[1].replaceAll("\"", "").trim());
+		}
+
+		Endereco endereco = new Endereco();
+		endereco.setEstado(mapa.get("uf"));
+		endereco.setCidade(mapa.get("localidade"));
+		endereco.setBairro(mapa.get("bairro"));
+		endereco.setLogradouro(mapa.get("logradouro"));
+		endereco.setCodIBGE(Integer.parseInt(mapa.get("ibge")));
+
+		return endereco;
+	}
 
 }
