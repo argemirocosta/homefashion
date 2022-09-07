776ed1e469cce4297461e41945e7f8eea76929bb
Argemiro github <argemirocosta@outlook.com>
2022-09-07 19:19:05 -0300

    Atualizando versão da dependencia do postgres e ajustando conexão com o heroku

--- src/main/br/com/homefashion/factory/ConexaoBuilder.java
+++ src/main/br/com/homefashion/factory/ConexaoBuilder.java
@@ -10,14 +10,15 @@ public class ConexaoBuilder {
         Propriedades propriedades = new Propriedades();
 
         Conexoes conexoes = new Conexoes();
-        if(propriedades.Conexao.equals(Propriedades.Conexoes.LOCALHOST)){
+        if(Propriedades.Conexao.equals(Propriedades.Conexoes.LOCALHOST)){
             conexoes.setUrlBanco("jdbc:postgresql://localhost:5432/home_fashion");
             conexoes.setUsuario("postgres");
             conexoes.setSenha("post");
         }
-        else if(propriedades.Conexao.equals(Propriedades.Conexoes.PRODUCAO)){
+        else if(Propriedades.Conexao.equals(Propriedades.Conexoes.PRODUCAO)){
             URI dbUri = new URI(System.getenv("DATABASE_URL"));
-            conexoes.setUrlBanco("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath());
+            conexoes.setUrlBanco("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+                    + "?sslmode=require");
             conexoes.setUsuario(dbUri.getUserInfo().split(":")[0]);
             conexoes.setSenha(dbUri.getUserInfo().split(":")[1]);
         }
